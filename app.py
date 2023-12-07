4399459dd0d4853d0e94c1498c7ad5908ec6a730
Emin Ozkan <emin.x.ozkan@gsk.com>
2023-12-07 15:20:33 -0500

    try deploy

--- app.py
+++ app.py
@@ -156,8 +156,8 @@ def logout():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    # app.run()
-    app.run(ssl_context="adhoc")
+    app.run()
+    # app.run(ssl_context="adhoc")
 
 # import os
 
