f09112888ed299f5edd8d06466bda1b44ac572f3
Emin Ozkan <emin.x.ozkan@gsk.com>
2023-12-20 11:01:42 -0500

    azure db work starts

--- db.py
+++ db.py
@@ -10,7 +10,7 @@ connection_string = os.environ["AZURE_SQL_CONNECTIONSTRING"]
 
 def get_db():
     if "db" not in g:
-        credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=False)
+        credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=True)
         token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
         token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
         SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
