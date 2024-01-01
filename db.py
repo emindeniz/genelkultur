cc4bd6023f820aa4d26ac90cb6ffd0bb82a67647
Emin Ozkan <emin.x.ozkan@gsk.com>
2024-01-01 12:22:35 -0500

    remove access token

--- db.py
+++ db.py
@@ -14,7 +14,9 @@ def get_db():
         token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
         token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
         SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
-        g.db = pyodbc.connect(connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
+        g.db = pyodbc.connect(connection_string, 
+                              # attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct}
+                              )
 
     return g.db
 
