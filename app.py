6f645a971a62182cc94a32b911a4058b4ec0c0e7
Emin Ozkan <emin.x.ozkan@gsk.com>
2024-01-01 12:58:33 -0500

    finished setting up azure db

--- app.py
+++ app.py
@@ -62,7 +62,6 @@ def index():
         )
     else:
         return '<a class="button" href="/login">Google Login</a>'
-        # return '<a href="/.auth/login/google?post_login_redirect_uri=/login/callback">Log in</a>'
     
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
