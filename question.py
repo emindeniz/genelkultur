e2c915ff7e794e8dfaf81b466a9fd08a64bd5955
Emin Ozkan <emin.x.ozkan@gsk.com>
2024-01-08 16:04:42 -0500

    get questions from db

---
+++ question.py
@@ -0,0 +1,30 @@
+from db import get_db
+
+class Question():
+    def __init__(self, id, question_text, answer, qtype, category):
+        self.id = id
+        self.question_text = question_text
+        self.answer = answer
+        self.qtype = qtype
+        self.category = category
+
+    @staticmethod
+    def get_random():
+        db = get_db()
+        question = db.execute(
+            f"""
+            SELECT top 1 * FROM [dbo].[questions] 
+            WHERE id >= RAND() * (SELECT MAX(id) FROM [dbo].[questions])
+            order by id;
+            """
+        ).fetchone()
+
+        question = Question(
+            id=question[0], 
+            question_text=question[1], 
+            answer=question[2], 
+            qtype=question[3],
+            category = question[4]
+        )
+
+        return question
